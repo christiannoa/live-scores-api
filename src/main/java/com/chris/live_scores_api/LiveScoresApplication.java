179b7ec1a5424f975693e7ef2c95ea81b221ed96
christiannoa <christiannoa20@gmail.com>
2025-10-06 22:13:43 -0400

    Initial Spring Boot setup for Live Scores API

---
+++ src/main/java/com/chris/live_scores_api/LiveScoresApplication.java
@@ -0,0 +1,13 @@
+package com.chris.live_scores_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveScoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveScoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chris/live_scores_api/LiveScoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.live_scores_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveScoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
